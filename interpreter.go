5695c2a680b3642f62f6857a0094ac420aa17526
Hwang In Tak <sp301415@gmail.com>
2022-08-13 20:05:45 +0900

    Remove buffered IO

--- interpreter.go
+++ interpreter.go
@@ -1,17 +1,11 @@
 package brainfuck
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
-
-var stdout = bufio.NewWriterSize(os.Stdout, 8192)
 
 // Execute executes a Program.
 func Execute(code Program) {
-	defer stdout.Flush()
-
 	MEM := [30000]int{}
 	ptr := 0
 
@@ -25,7 +19,7 @@ func Execute(code Program) {
 		case Scan:
 			fmt.Scanf("%c", &MEM[ptr])
 		case Print:
-			fmt.Fprintf(stdout, "%c", MEM[ptr])
+			fmt.Printf("%c", MEM[ptr])
 		case JumpIf:
 			if MEM[ptr] == 0 {
 				pc = instruction.Operand
